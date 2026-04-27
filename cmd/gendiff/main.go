591e0a13789208b3a0d3c955b733ac491caf867c
artem.ivanov <artem.ivanov@indels.org>
2026-04-27 22:10:13 +0900

    feat: update gendiff return

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -41,7 +41,7 @@ func main() {
 				return err
 			}
 
-			fmt.Printf("%s", *result)
+			fmt.Printf("%s", result)
 
 			return nil
 		},
--- gendiff.go
+++ gendiff.go
@@ -21,48 +21,48 @@ var (
 	ErrEmptyPath = errors.New("file path cannot be empty")
 )
 
-func GenDiff(filepath1, filepath2 string, format string) (*string, error) {
+func GenDiff(filepath1, filepath2 string, format string) (string, error) {
 	if filepath1 == "" {
-		return nil, fmt.Errorf("first file: %w", ErrEmptyPath)
+		return "", fmt.Errorf("first file: %w", ErrEmptyPath)
 	}
 	if filepath2 == "" {
-		return nil, fmt.Errorf("second file: %w", ErrEmptyPath)
+		return "", fmt.Errorf("second file: %w", ErrEmptyPath)
 	}
 
 	data1, err := readFile(filepath1)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	data2, err := readFile(filepath2)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	ext1 := filepath.Ext(filepath1)
 	ext2 := filepath.Ext(filepath2)
 
 	if !strings.EqualFold(ext1, ext2) {
-		return nil, fmt.Errorf("files have different extensions")
+		return "", fmt.Errorf("files have different extensions")
 	}
 
 	parsed1, err := parsers.Parse(data1, ext1)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	parsed2, err := parsers.Parse(data2, ext2)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	nodes := differ.BuildDiffNodes(parsed1, parsed2)
 	formattedNodes, err := formatters.FormatNodes(nodes, format, 1)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return formattedNodes, nil
+	return *formattedNodes, nil
 }
 
 func readFile(path string) ([]byte, error) {
